fd1966375813f60f188f735c89553eed05c5a76a
Melanie Rodas <melanierltel@gmail.com>
2024-10-11 11:03:09 -0500

    fix: resolve import error by set up path aliases in vite.config.ts and install @types/node to resolve module path issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@root": path.resolve(__dirname, "src"),
+    },
+  },
   plugins: [react()],
   server: { port: 3000, open: true },
 });
