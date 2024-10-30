7160bde5cf9dc2813263460af0094b6f2819a3dd
Melanie Rodas <melanierltel@gmail.com>
2024-10-30 18:58:50 -0500

    fix: update useDistricts test

--- src/Hooks/__test__/useDistricts.test.ts
+++ src/Hooks/__test__/useDistricts.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import useDistricts from "@root/hooks/useDistricts";
 import districtsData from "@root/data/districts-data.js";
 
